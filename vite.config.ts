18192077010dcfae659be5259eb38ba5a21e2ac0
Rita Fetsch <ritamelanie5@gmail.com>
2025-04-10 08:32:45 +0100

    updates for deploy strategy

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' 
-    ? '/ritafetsch.github.io/' 
-    : '/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
